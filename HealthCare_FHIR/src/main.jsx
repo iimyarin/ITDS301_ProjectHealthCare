b9e016b243ee2e0d377c6ccc3c21eb71d848a57f
kullanipaJag <kullanipa.jag@student.mahidol.edu>
2024-05-25 21:57:26 +0700

    DoctotForm Updated

--- HealthCare_FHIR/src/main.jsx
+++ HealthCare_FHIR/src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import PatientForm from './Patientform.jsx'
 import LabForm from "./Labform.jsx"
+import DoctorForm from "./DoctorForm.jsx"
 import './index.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
@@ -11,6 +12,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <Routes>
         <Route path="/patient" element={<PatientForm />} />
         <Route path="/lab" element={<LabForm />} />
+        <Route path="/doctor" element={<DoctorForm />} />
       </Routes>
     </React.StrictMode>
   </Router>
